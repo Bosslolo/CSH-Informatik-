80899ed9c819b0fd3bb0eb584b555a62515ff0fc
Bosslolo <bosslolo2.22@gmail.com>
2026-04-29 16:54:46 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3003;
 
 // Middleware for JSON (needed for potential POST later)
 app.use(express.json());
